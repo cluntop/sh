9ceaad3517aed1c1ea461e30ab7e4fe12fbb4e22
雨呢 <85211716+cluntop@users.noreply.github.com>
2025-01-25 00:25:15 +0800

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -1,1 +1,1 @@
-sleep 1 && curl -o clun_tcp.sh https://raw.githubusercontent.com/cluntop/cluntop.github.io/main/tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh
+curl -o clun_tcp.sh https://raw.githubusercontent.com/cluntop/cluntop.github.io/main/tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh
