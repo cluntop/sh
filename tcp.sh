a1825b2407d05afb84c81ff78153c570ff93785e
ClunTop <85211716+cluntop@users.noreply.github.com>
2025-02-27 04:48:40 +0800

    更新 tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -235,10 +235,10 @@ net.ipv4.tcp_mem = $tcp_low $tcp_mid $tcp_high
 net.ipv4.udp_mem = $udp_low $udp_mid $udp_high
 
 # 全局套接字默认接受缓冲区 # 212992
-net.core.rmem_default = 524288
+net.core.rmem_default = 8388608
 net.core.rmem_max = 536870912
 # 全局套接字默认发送缓冲区 # 212992
-net.core.wmem_default = 524288
+net.core.wmem_default = 8388608
 net.core.wmem_max = 536870912
 # 控制单个套接字（socket）可分配的附加选项内存的最大值。
 net.core.optmem_max = 67108864
