c67ac745f6c861ca521433e83dd4097ec774899e
ClunTop <85211716+cluntop@users.noreply.github.com>
2025-05-23 22:13:46 +0800

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -4,7 +4,7 @@
 # curl https://raw.githubusercontent.com/cluntop/sh/main/tcp.sh -o clun_tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh
 
 version="1.1.1"
-version_test="143"
+version_test="144"
 
 RED='\033[31m'
 GREEN='\033[32m'
@@ -150,11 +150,11 @@ vm.max_map_count = 262144
 net.ipv4.tcp_shrink_window = 1
 net.ipv4.tcp_collapse_max_bytes = 6291456
 
-# 全局套接字默认接受缓冲区 # 212992 # 212992
-net.core.rmem_default = 26214400
+# 全局套接字默认接受缓冲区 # 212992 # 212992 #26214400
+net.core.rmem_default = 4194304
 net.core.rmem_max = 536870912
-# 全局套接字默认发送缓冲区 # 212992 # 212992
-net.core.wmem_default = 26214400
+# 全局套接字默认发送缓冲区 # 212992 # 212992 #26214400
+net.core.wmem_default = 4194304
 net.core.wmem_max = 536870912
 # 控制单个套接字（socket）可分配的附加选项内存的最大值。
 net.core.optmem_max = 262144
