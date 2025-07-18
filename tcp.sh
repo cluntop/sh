ea6eee3c1ff6aa25d1ded4cae04a99f02be465f3
cluntop <85211716+cluntop@users.noreply.github.com>
2025-07-18 18:51:43 +0800

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -3,7 +3,7 @@
 # bash <(curl -sL clun.top)
 
 version="1.1.7"
-version_test="178"
+version_test="179"
 
 RED='\033[31m'
 GREEN='\033[32m'
@@ -23,11 +23,11 @@ tcp_low=$(echo "$size_mb * 1024 / 32" | bc)
 tcp_mid=$(echo "$size_mb * 1024 / 16" | bc)
 tcp_high=$(echo "$size_mb * 1024 / 8" | bc)
 
-udp_low=$(echo "$size_mb * 1024 / 19" | bc)
+udp_low=$(echo "$size_mb * 1024 / 30" | bc)
 udp_mid=$(echo "$size_mb * 1024 / 14" | bc)
-udp_high=$(echo "$size_mb * 1024 / 9" | bc)
+udp_high=$(echo "$size_mb * 1024 / 6" | bc)
 
-conntrack_max=$(echo "$size_mb * 16384 / 4" | bc)
+conntrack_max=$(echo "$size_mb * 65536 / 4" | bc)
 
 tcp_dyjs=$(sudo dmidecode -t memory | grep -i "Size:" | sed -e '/No Module Installed/d' -e 's/.*Size: \([0-9]\+\).*/\1/')
 tcp_dy=$(echo "$tcp_dyjs * 16 / 2" | bc)
@@ -156,38 +156,38 @@ net.ipv4.tcp_early_retrans = 1
 
 # 全局套接字默认接受缓冲区
 # 212992 # 212992 #26214400
-net.core.rmem_default = 87380
+net.core.rmem_default = 262144
 net.core.rmem_max = 536870912
 
 # 全局套接字默认发送缓冲区
 # 212992 # 212992 #26214400
-net.core.wmem_default = 65536
+net.core.wmem_default = 262144
 net.core.wmem_max = 536870912
 
 # 控制单个套接字（socket）
 net.core.optmem_max = 262144
 
 # 缓冲区相关配置均和内存相关 # 6291456
-net.ipv4.tcp_rmem = 8192 250000 536870912
-net.ipv4.tcp_wmem = 4096 65536 536870912
+net.ipv4.tcp_rmem = 65536 262144 536870912
+net.ipv4.tcp_wmem = 65536 262144 536870912
 net.ipv4.ip_local_port_range = 1024 65535
 net.ipv4.tcp_adv_win_scale = -2
 
 # 半连接队列大小（SYN 队列）
-net.ipv4.tcp_max_syn_backlog = 350000
+net.ipv4.tcp_max_syn_backlog = 250000
 
 # 网卡接收队列大小（所有协议数据包）
-net.core.netdev_max_backlog = 250000
+net.core.netdev_max_backlog = 131072
 
 # 全连接队列大小（Accept 队列）
-net.core.somaxconn = 655360
+net.core.somaxconn = 65536
 
-# 配置TCP/IP协议栈。控制在TCP接收缓冲区溢出时的行为。
-net.ipv4.tcp_abort_on_overflow = 1
+# 控制 TCP 连接队列溢出行为的核心参数
+net.ipv4.tcp_abort_on_overflow = 0
 
 # 所有网卡每次软中断最多处理的总帧数量
-net.core.netdev_budget = 50000
-net.core.netdev_budget_usecs = 5000
+net.core.netdev_budget = 70000
+net.core.netdev_budget_usecs = 1000
 
 # TCP 自动窗口
 # 要支持超过 64KB 的 TCP 窗口必须启用
@@ -210,15 +210,15 @@ net.ipv4.tcp_tw_reuse = 1
 
 # 系统同时保持TIME_WAIT套接字的最大数量
 # 如果超过这个数字 TIME_WAIT 套接字将立刻被清除
-net.ipv4.tcp_max_tw_buckets = 102400
+net.ipv4.tcp_max_tw_buckets = 1024000
 
 # 启用选择应答
 # 对于广域网通信应当启用
-net.ipv4.tcp_sack = 0
+net.ipv4.tcp_sack = 1
 
 # 启用转发应答
 # 对于广域网通信应当启用
-net.ipv4.tcp_fack = 0
+net.ipv4.tcp_fack = 1
 
 # 开启F-RTO(针对TCP重传超时的增强的恢复算法).
 net.ipv4.tcp_frto = 0
@@ -227,8 +227,8 @@ net.ipv4.tcp_frto = 0
 net.ipv4.tcp_ecn = 0
 
 # TCP SYN 连接超时重传次数
-net.ipv4.tcp_syn_retries = 3
-net.ipv4.tcp_synack_retries = 3
+net.ipv4.tcp_synack_retries = 5
+net.ipv4.tcp_syn_retries = 4
 
 # TCP SYN 连接超时时间, 设置为 5 约为 30s
 # 放弃回应一个 TCP 连接请求前, 需要进行多少次重试
@@ -247,7 +247,7 @@ net.ipv4.conf.all.rp_filter = 1
 
 # 减少处于 FIN-WAIT-2
 # 连接状态的时间使系统可以处理更多的连接
-net.ipv4.tcp_fin_timeout = 120
+net.ipv4.tcp_fin_timeout = 30
 
 # unix socket 最大队列
 net.unix.max_dgram_qlen = 65536
@@ -260,6 +260,12 @@ net.ipv4.icmp_echo_ignore_all = 1
 
 # 取消对广播 ICMP 包的回应
 net.ipv4.icmp_echo_ignore_broadcasts = 1
+
+# 控制 ICMP 错误消息的源地址选择逻辑
+net.ipv4.icmp_errors_use_inbound_ifaddr = 1
+
+# 开启恶意 ICMP 错误消息保护
+net.ipv4.icmp_ignore_bogus_error_responses = 1
 
 # TCP基础最大报文段大小 MSS
 net.ipv4.tcp_base_mss = 1460
@@ -342,14 +348,17 @@ vm.swappiness = 10
 vm.overcommit_memory = 1
 vm.overcommit_ratio = 80
 
-# 增加系统文件描述符限制
-# Fix error: too many open files
-fs.file-max = 102400000
-fs.inotify.max_user_instances = 65536
+fs.nr_open = 1024000
+fs.file-max = 1024000
 
 # 设置 inotify 监视的最大用户监视器数量。
 fs.inotify.max_user_watches = 524288
-fs.nr_open = 1024000
+
+# 增加系统文件描述符限制
+# Fix error: too many open files
+fs.inotify.max_user_instances = 524288
+
+kernel.pid_max = 65536
 
 # 内核响应魔术键
 kernel.sysrq = 0
@@ -368,7 +377,7 @@ vm.zone_reclaim_mode = 0
 
 # TCP FastOpen
 net.ipv4.tcp_fastopen = 3
-net.ipv4.tcp_fastopen_blackhole_timeout_sec = 1
+net.ipv4.tcp_fastopen_blackhole_timeout_sec = 15
 
 # TCP 流中重排序的数据报最大数量
 net.ipv4.tcp_reordering = 5
@@ -387,9 +396,6 @@ net.ipv4.tcp_tso_win_divisor = 3
 
 # 控制 TCP 协议在处理 TIME-WAIT 状态时的行为
 net.ipv4.tcp_rfc1337 = 0
-
-# 开启恶意 ICMP 错误消息保护
-net.ipv4.icmp_ignore_bogus_error_responses = 1
 
 # 控制未解析（unresolved）的邻居（neighbor）项队列长度。
 # net.ipv4.neigh.default.unres_qlen = 10
@@ -414,13 +420,13 @@ net.ipv4.tcp_thin_linear_timeouts = 0
 net.ipv4.tcp_pacing_ca_ratio = 110
 
 # 作用：UDP队列里数据报的最大个数
-net.unix.max_dgram_qlen = 100
+net.unix.max_dgram_qlen = 1024
 
 # 作用：内核的随机地址保护模式
 kernel.randomize_va_space = 1
 
 # Linux 内核中用于控制系统信号量（Semaphore）的参数。
-kernel.sem = 512 32000 100 256
+kernel.sem = 512 262144 300 1024
 
 # 虚拟化优化（KVM 环境）
 kernel.sched_min_granularity_ns = 10000000
